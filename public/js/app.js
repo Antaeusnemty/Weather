96596019966331237b1be9d67e1d9b3e428c09af
Antaeusnemty <gamingemail007@gmail.com>
2015-10-01 18:58:04 -0400

    setup a basic get request for weather data

--- public/js/app.js
+++ public/js/app.js
@@ -5,8 +5,22 @@ app.controller("TestController", function($scope){
     $scope.test = "It works";
 });
 
-app.controller("WeatherController", function($scope){
+app.controller("WeatherController", function($scope, $http){
     $scope.name = "Weather";
+    $scope.weatherData = {};
+    var baseUrl = "https://api.forecast.io/forecast/";
+    var baseUrlwApi = baseUrl + apiKey+"/";
+    
+    //http GET 
+    $http.jsonp(baseUrlwApi+"37.8267,-122.423?callback=JSON_CALLBACK")
+        .success(function(response){
+            $scope.weatherData = response;
+            console.log($scope.weatherData);
+    })
+        .error(function(err){
+            console.log(err);
+    });
+    
 });
 app.controller("HomeController", function($scope){
 

39c3179228cf5e6fce8c0bef983c53f0716c7825
Antaeusnemty <gamingemail007@gmail.com>
2015-10-05 19:50:22 -0400

    Rebuilt to use weather service

--- public/js/app.js
+++ public/js/app.js
@@ -19,5 +19,15 @@ app.config(function($routeProvider){
         
 });
 
+app.factory("weatherService", function($http){
+    var service = {};
+     var baseUrl = "https://api.forecast.io/forecast/";
+      var baseUrlwApi = baseUrl + apiKey+"/";
+    //this is going to get the weather info
+    service.get = function(latitude, longitude){
+    //http GET 
+return $http.jsonp(baseUrlwApi+latitude+","+longitude+"?callback=JSON_CALLBACK")
+    };
 
-
+    return service;
+});
--- public/js/controllers/WeatherController.js
+++ public/js/controllers/WeatherController.js
@@ -1,22 +1,22 @@
-app.controller("WeatherController", function($scope, $http){
+app.controller("WeatherController", function($scope, weatherService){
     $scope.latitude = 0;
     $scope.longitude = 0;
     $scope.weatherData = {};
-    var baseUrl = "https://api.forecast.io/forecast/";
-    var baseUrlwApi = baseUrl + apiKey+"/";
+    $scope.showWeather = false;
+   
     
     //getWeather function
     $scope.getWeather = function(){
-    
-        //http GET 
-        $http.jsonp(baseUrlwApi+$scope.latitude+","+$scope.longitude+"?callback=JSON_CALLBACK")
-            .success(function(response){
-                $scope.weatherData = response;
-                console.log($scope.weatherData);
-    })
-            .error(function(err){
-              console.log(err);
-    });
+     weatherService.get($scope.latitude, $scope.longitude)
+        .success(function(response){
+          $scope.weatherData = response;
+            $scope.showWeather = true;
+      
+         })
+        .error(function(err){
+          console.log(err);
+            $scope.showWeather = false;
+        });
     };
-    
-     });
+});
+

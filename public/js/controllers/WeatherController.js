14c3783cf36f1a28475445d36ece498900788f81
Antaeusnemty <gamingemail007@gmail.com>
2015-10-06 19:30:23 -0400

    Changed lat and lng to location

--- public/js/controllers/WeatherController.js
+++ public/js/controllers/WeatherController.js
@@ -1,22 +1,50 @@
-app.controller("WeatherController", function($scope, weatherService){
-    $scope.latitude = 0;
-    $scope.longitude = 0;
+app.controller("WeatherController", function($scope, $http, weatherService){
+ //   $scope.latitude = 0;
+//    $scope.longitude = 0;
+    $scope.location = "";
     $scope.weatherData = {};
     $scope.showWeather = false;
    
     
     //getWeather function
     $scope.getWeather = function(){
-     weatherService.get($scope.latitude, $scope.longitude)
+        var latitude = 0;
+        var longitude = 0;
+        
+        $http.get("https://maps.googleapis.com/maps/api/geocode/json?address="+$scope.location+"&key=AIzaSyAKIX_clokWdTnhvueWf9GW0m9ufXjU-tU")
+        .success(function(response){
+          latitude = response
+              .results[0]
+              .geometry
+              .location
+              .lat;
+            longitude = response
+              .results[0]
+              .geometry
+              .location
+              .lng;
+            console.log(latitude);
+            console.log(longitude);
+         })
+        .error(function(err){
+         console.log(err)
+        })
+        weatherService.get(latitude, longitude)
         .success(function(response){
           $scope.weatherData = response;
             $scope.showWeather = true;
       
          })
+        //weather service
+                                      //       weatherService.get(latitude, longitude)
+                                        //        .success(function(response){
+                                    //              $scope.weatherData = response;
+                                      //              $scope.showWeather = true;
+
+                                        //         })
         .error(function(err){
           console.log(err);
             $scope.showWeather = false;
         });
     };
 });
-
